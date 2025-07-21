0bc70a664c4a4a5dada58e1a5598aee9a4987994
Sarad Babu Shrestha <saradshrestha001@gmail.com>
2025-07-21 08:20:50 +0545

    routing setup

---
+++ app/components/hello.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+function Hello() {
+  console.log("Hello i am a client component");
+  return <h1 className="text-3xl">Hello</h1>;
+}
+export default Hello;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
-import Image from "next/image";
+import Hello from "./components/hello";
 
 export default function Home() {
-  return <h1 className="text-3xl">Welcome to Next.js</h1>;
+  console.log("What am i doing? -- SERVER");
+  return (
+    <>
+      <h1 className="text-3xl">Welcome to Next.js</h1>
+      <Hello />
+    </>
+  );
 }
