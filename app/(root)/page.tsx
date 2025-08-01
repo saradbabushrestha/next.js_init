0978a7d67a3861fca2d0d257410363402f62a4af
Sarad Babu Shrestha <saradshrestha001@gmail.com>
2025-08-01 09:55:42 +0545

    All Startup and Startup Card setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
 export default async function Home({
@@ -6,6 +7,18 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Sarad" },
+      _id: 1,
+      description: "This is a description",
+      image: "https://i.postimg.cc/FHNsG1k5/Meg-Pup-Sticker-copy.jpg",
+      category: "Frog",
+      title: "Umberella & Frog",
+    },
+  ];
   return (
     <>
       <section className="pink_container">
@@ -17,6 +30,20 @@ export default async function Home({
           Competitions.
         </p>
         <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Showing results for ${query}` : "All Startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={posts?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No Startup Found</p>
+          )}
+        </ul>
       </section>
     </>
   );
